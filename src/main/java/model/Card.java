2298de2dae838137827c75a8310f9ecb75e96736
amtei <evgeiyryshenko@gmail.com>
2023-12-10 23:27:47 +0200

    создал генератор для cvc и number

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,41 +1,60 @@
 package model;
 
+import java.time.LocalDate;
+
+
 public class Card {
-    private int number;
-    private int date;
-    public String Person;
-    private int cvc;
+    private String number;
+    private String cvc;
+    public String name;
 
-    public Card(String person ){
-        this.Person=person;
-    }
-    public void setNumber(int number) {
-        this.number = number;
-    }
+    public LocalDate date ;
 
-    public int getNumber() {
-        return number;
-    }
 
-    public void setDate(int date) {
-        this.date = date;
+    public Card(String name) {
+        this.name = name;
+        this.cvc = generateCVC();
+        this.number = generateNumber();
+        this.date=LocalDate.now().plusYears(3);
+
     }
 
-    public int getDate() {
-        return date;
+    private String generateNumber() {  int i = (int) (Math.random() * 9);
+        String str=Integer.toString(i);
+
+        int a = (int) (Math.random() * 9);
+
+        int b = (int) (Math.random() * 9);
+        int c = (int) (Math.random() * 9);
+        int v = (int) (Math.random() * 9);
+        int d = (int) (Math.random() * 9);
+        int e = (int) (Math.random() * 9);
+        int x = (int) (Math.random() * 9);
+        int o = (int) (Math.random() * 9);
+        StringBuilder af = new StringBuilder(str);
+        af.append(a).append(b).append(c).append(v).append(d).append(e).append(x).append(o);
+        return ""+af;
+
+
     }
 
-    public void setCvc(int cvc) {
-        this.cvc = cvc;
+    private String generateCVC() {
+        int i = (int) (Math.random() * 9);
+        String str=Integer.toString(i);
+
+        int a = (int) (Math.random() * 9);
+
+        int b = (int) (Math.random() * 9);
+
+        StringBuilder af = new StringBuilder(str);
+        af.append(a).append(b);
+        return ""+af;
     }
 
-    public int getCvc() {
-        return cvc;
+    public void info(){
+        System.out.println(cvc);
+        System.out.println();
+        System.out.println(number);
     }
-//        public void info(){
-//        System.out.println("my number" +number);
-//            System.out.println(cvc);
-//            System.out.println(date);
-//    }
 }
 
